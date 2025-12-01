5faa6bb5cd2161b4b576dede2dddc1ac2400673b
Adrian Ojeda <adrian.ojeda103@alu.ulpgc.es>
2025-12-01 13:13:24 +0000

    some changes

--- run.py
+++ run.py
@@ -1,13 +1,41 @@
-# Search methods
-
 import search
+import time
 
-ab = search.GPSProblem('A', 'B'
-                       , search.romania)
+def main():
+    # Definimos el problema: Arad -> Bucharest
+    ab = search.GPSProblem('A', 'B', search.romania)
+    
+    # Lista de algoritmos a ejecutar
+    strategies = [
+        ("Anchura (BFS)", 'bfs'),
+        ("Profundidad (DFS)", 'dfs'),
+        ("Ramif. y Acot. (B&B)", 'bab'), # 
+        ("A* (Heurística)", 'astar')    # [cite: 16]
+    ]
 
-print(search.breadth_first_graph_search(ab).path())
-print(search.depth_first_graph_search(ab).path())
+    print("\n--- RESULTADOS PRÁCTICA 1 (FSI) ---")
+    # Cabecera de la tabla 
+    print(f"{'ESTRATEGIA':<25} | {'GEN':<6} | {'VIS':<6} | {'COST':<6} | RUTA")
+    print("-" * 90)
 
-# Result:
-# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
-# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
+    for name, code in strategies:
+        start_time = time.time()
+        
+        # Ejecutamos usando la versión instrumentada
+        node, gen, vis = search.run_search_with_metrics(ab, code)
+        
+        elapsed = time.time() - start_time
+        
+        if node:
+            # Reconstruir camino para imprimir (Invertir porque .path() va de fin a inicio)
+            path_objs = node.path()
+            path_names = [n.state for n in reversed(path_objs)]
+            cost = node.path_cost
+            
+            # Imprimir fila de la tabla
+            print(f"{name:<25} | {gen:<6} | {vis:<6} | {cost:<6} | {path_names}")
+        else:
+            print(f"{name:<25} | -      | -      | -      | No encontrada")
+
+if __name__ == "__main__":
+    main()
--- search.py
+++ search.py
@@ -1,82 +1,43 @@
-"""Search (Chapters 3-4)
-
-The way to use this code is to subclass Problem to create a class of problems,
-then create problem instances and solve them with calls to the various search
-functions."""
-
-
-from utils import *
-import random
-import sys
-
+from utils import * 
 
 # ______________________________________________________________________________
-
+# CÓDIGO BASE (Respetando interfaces originales) [cite: 8]
 
 class Problem:
-    """The abstract class for a formal problem.  You should subclass this and
-    implement the method successor, and possibly __init__, goal_test, and
-    path_cost. Then you will create instances of your subclass and solve them
-    with the various search functions."""
-
     def __init__(self, initial, goal=None):
-        """The constructor specifies the initial state, and possibly a goal
-        state, if there is a unique goal.  Your subclass's constructor can add
-        other arguments."""
         self.initial = initial
         self.goal = goal
 
     def successor(self, state):
-        """Given a state, return a sequence of (action, state) pairs reachable
-        from this state. If there are many successors, consider an iterator
-        that yields the successors one at a time, rather than building them
-        all at once. Iterators will work fine within the framework."""
-        abstract
+        abstract()
 
     def goal_test(self, state):
-        """Return True if the state is a goal. The default method compares the
-        state to self.goal, as specified in the constructor. Implement this
-        method if checking against a single self.goal is not enough."""
         return state == self.goal
 
     def path_cost(self, c, state1, action, state2):
-        """Return the cost of a solution path that arrives at state2 from
-        state1 via action, assuming cost c to get up to state1. If the problem
-        is such that the path doesn't matter, this function will only look at
-        state2.  If the path does matter, it will consider c and maybe state1
-        and action. The default method costs 1 for every step in the path."""
         return c + 1
 
     def value(self):
-        """For optimization problems, each state has a value.  Hill-climbing
-        and related algorithms try to maximize this value."""
-        abstract
-
-
-# ______________________________________________________________________________
+        abstract()
 
 class Node:
-    """A node in a search tree. Contains a pointer to the parent (the node
-    that this is a successor of) and to the actual state for this node. Note
-    that if a state is arrived at by two paths, then there are two nodes with
-    the same state.  Also includes the action that got us to this state, and
-    the total path_cost (also known as g) to reach the node.  Other functions
-    may add an f and h value; see best_first_graph_search and astar_search for
-    an explanation of how the f and h values are handled. You will not need to
-    subclass this class."""
-
     def __init__(self, state, parent=None, action=None, path_cost=0):
-        """Create a search tree Node, derived from a parent by an action."""
-        update(self, state=state, parent=parent, action=action,
-               path_cost=path_cost, depth=0)
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+        self.depth = 0
         if parent:
             self.depth = parent.depth + 1
 
     def __repr__(self):
         return "<Node %s>" % (self.state,)
+    
+    # Añadido: Necesario para que PriorityQueue compare nodos en caso de empate de costes
+    def __lt__(self, other):
+        return self.state < other.state
 
     def path(self):
-        """Create a list of nodes from the root to this node."""
         x, result = self, [self]
         while x.parent:
             result.append(x.parent)
@@ -84,62 +45,81 @@ class Node:
         return result
 
     def expand(self, problem):
-        """Return a list of nodes reachable from this node. [Fig. 3.8]"""
-        return [Node(next, self, act,
-                     problem.path_cost(self.path_cost, self.state, act, next))
-                for (act, next) in problem.successor(self.state)]
-
+        return [Node(next_state, self, act,
+                     problem.path_cost(self.path_cost, self.state, act, next_state))
+                for (act, next_state) in problem.successor(self.state)]
 
 # ______________________________________________________________________________
-## Uninformed Search algorithms
+# MODIFICACIÓN: Búsqueda instrumentada (Parte 3) 
 
-def graph_search(problem, fringe):
-    """Search through the successors of a problem to find a goal.
-    The argument fringe should be an empty queue.
-    If two paths reach a state, only use the best one. [Fig. 3.18]"""
-    closed = {}
-    fringe.append(Node(problem.initial))
-    while fringe:
+def graph_search_instrumented(problem, fringe):
+    """
+    Versión modificada de graph_search que cuenta nodos.
+    Retorna: (NodeSolucion, NodosGenerados, NodosVisitados)
+    """
+    closed = set() # Usamos set en vez de dict para eficiencia, compatible con lógica original
+    
+    # Contadores 
+    generated_nodes = 0
+    visited_nodes = 0
+    
+    initial_node = Node(problem.initial)
+    fringe.append(initial_node)
+    generated_nodes += 1 # [cite: 24] Nodos creados
+    
+    while len(fringe) > 0:
         node = fringe.pop()
+        visited_nodes += 1 # [cite: 26] Nodos interrogados (sacados de la frontera)
+        
         if problem.goal_test(node.state):
-            return node
+            return node, generated_nodes, visited_nodes
+        
         if node.state not in closed:
-            closed[node.state] = True
-            fringe.extend(node.expand(problem))
-    return None
+            closed.add(node.state)
+            successors = node.expand(problem)
+            fringe.extend(successors)
+            generated_nodes += len(successors) # Sumamos los generados al expandir
+            
+    return None, generated_nodes, visited_nodes
 
+# Wrapper original para NO romper compatibilidad [cite: 8]
+def graph_search(problem, fringe):
+    result, _, _ = graph_search_instrumented(problem, fringe)
+    return result
+
+# ______________________________________________________________________________
+# ESTRATEGIAS DE BÚSQUEDA
 
 def breadth_first_graph_search(problem):
-    """Search the shallowest nodes in the search tree first. [p 74]"""
-    return graph_search(problem, FIFOQueue())  # FIFOQueue -> fringe
-
+    return graph_search(problem, FIFOQueue())
 
 def depth_first_graph_search(problem):
-    """Search the deepest nodes in the search tree first. [p 74]"""
     return graph_search(problem, Stack())
 
+# Parte 1: Ramificación y Acotación 
+def branch_and_bound_graph_search(problem):
+    # Ordena por coste de camino (g)
+    return graph_search(problem, PriorityQueue(order='min', f=lambda n: n.path_cost))
 
+# Parte 2: A* (Subestimación) [cite: 16]
+def astar_graph_search(problem):
+    # Ordena por f(n) = g(n) + h(n)
+    return graph_search(problem, PriorityQueue(order='min', f=lambda n: n.path_cost + problem.h(n)))
 
-# _____________________________________________________________________________
-# The remainder of this file implements examples for the search algorithms.
+# Funciones "Helper" para obtener las métricas en run.py sin romper las funciones originales
+def run_search_with_metrics(problem, algorithm_name):
+    fringe = None
+    if algorithm_name == 'bfs': fringe = FIFOQueue()
+    elif algorithm_name == 'dfs': fringe = Stack()
+    elif algorithm_name == 'bab': fringe = PriorityQueue(order='min', f=lambda n: n.path_cost)
+    elif algorithm_name == 'astar': fringe = PriorityQueue(order='min', f=lambda n: n.path_cost + problem.h(n))
+    
+    return graph_search_instrumented(problem, fringe)
 
 # ______________________________________________________________________________
-# Graphs and Graph Problems
+# DEFINICIÓN DE GRAFOS (Copiado del base para mantener autonomía)
 
 class Graph:
-    """A graph connects nodes (vertices) by edges (links).  Each edge can also
-    have a length associated with it.  The constructor call is something like:
-        g = Graph({'A': {'B': 1, 'C': 2})
-    this makes a graph with 3 nodes, A, B, and C, with an edge of length 1 from
-    A to B,  and an edge of length 2 from A to C.  You can also do:
-        g = Graph({'A': {'B': 1, 'C': 2}, directed=False)
-    This makes an undirected graph, so inverse links are also added. The graph
-    stays undirected; if you add more links with g.connect('B', 'C', 3), then
-    inverse link is also added.  You can use g.nodes() to get a list of nodes,
-    g.get('A') to get a dict of links out of A, and g.get('A', 'B') to get the
-    length of the link from A to B.  'Lengths' can actually be any object at
-    all, and nodes can be any hashable object."""
-
     def __init__(self, dict=None, directed=True):
         self.dict = dict or {}
         self.directed = directed
@@ -147,70 +127,22 @@ class Graph:
             self.make_undirected()
 
     def make_undirected(self):
-        """Make a digraph into an undirected graph by adding symmetric edges."""
         for a in list(self.dict.keys()):
-            for (b, distance) in list(self.dict[a].items()):
-                self.connect1(b, a, distance)
-
-    def connect(self, A, B, distance=1):
-        """Add a link from A and B of given distance, and also add the inverse
-        link if the graph is undirected."""
-        self.connect1(A, B, distance)
-        if not self.directed: self.connect1(B, A, distance)
+            for (b, dist) in list(self.dict[a].items()):
+                self.connect1(b, a, dist)
 
     def connect1(self, A, B, distance):
-        """Add a link from A to B of given distance, in one direction only."""
         self.dict.setdefault(A, {})[B] = distance
 
     def get(self, a, b=None):
-        """Return a link distance or a dict of {node: distance} entries.
-        .get(a,b) returns the distance or None;
-        .get(a) returns a dict of {node: distance} entries, possibly {}."""
         links = self.dict.setdefault(a, {})
-        if b is None:
-            return links
-        else:
-            return links.get(b)
-
-    def nodes(self):
-        """Return a list of nodes in the graph."""
-        return list(self.dict.keys())
-
+        if b is None: return links
+        return links.get(b)
 
 def UndirectedGraph(dict=None):
-    """Build a Graph where every edge (including future ones) goes both ways."""
     return Graph(dict=dict, directed=False)
 
-
-def RandomGraph(nodes=list(range(10)), min_links=2, width=400, height=300,
-                curvature=lambda: random.uniform(1.1, 1.5)):
-    """Construct a random graph, with the specified nodes, and random links.
-    The nodes are laid out randomly on a (width x height) rectangle.
-    Then each node is connected to the min_links nearest neighbors.
-    Because inverse links are added, some nodes will have more connections.
-    The distance between nodes is the hypotenuse times curvature(),
-    where curvature() defaults to a random number between 1.1 and 1.5."""
-    g = UndirectedGraph()
-    g.locations = {}
-    ## Build the cities
-    for node in nodes:
-        g.locations[node] = (random.randrange(width), random.randrange(height))
-    ## Build roads from each city to at least min_links nearest neighbors.
-    for i in range(min_links):
-        for node in nodes:
-            if len(g.get(node)) < min_links:
-                here = g.locations[node]
-
-                def distance_to_node(n):
-                    if n is node or g.get(node, n): return infinity
-                    return distance(g.locations[n], here)
-
-                neighbor = argmin(nodes, distance_to_node)
-                d = distance(g.locations[neighbor], here) * curvature()
-                g.connect(node, neighbor, int(d))
-    return g
-
-
+# Mapa de Rumanía (Base code data)
 romania = UndirectedGraph(Dict(
     A=Dict(Z=75, S=140, T=118),
     B=Dict(U=85, P=101, G=90, F=211),
@@ -224,7 +156,9 @@ romania = UndirectedGraph(Dict(
     O=Dict(Z=71, S=151),
     P=Dict(R=97),
     R=Dict(S=80),
-    U=Dict(V=142)))
+    U=Dict(V=142),
+    Z=Dict(O=71))) # Asegurado Z
+
 romania.locations = Dict(
     A=(91, 492), B=(400, 327), C=(253, 288), D=(165, 299),
     E=(562, 293), F=(305, 449), G=(375, 270), H=(534, 350),
@@ -237,28 +171,27 @@ australia = UndirectedGraph(Dict(
     SA=Dict(WA=1, NT=1, Q=1, NSW=1, V=1),
     NT=Dict(WA=1, Q=1),
     NSW=Dict(Q=1, V=1)))
-australia.locations = Dict(WA=(120, 24), NT=(135, 20), SA=(135, 30),
-                           Q=(145, 20), NSW=(145, 32), T=(145, 42), V=(145, 37))
 
+australia.locations = Dict(
+WA=(120, 24), NT=(135, 20), SA=(135, 30),
+Q=(145, 20), NSW=(145, 32), T=(145, 42), V=(145, 37))
 
 class GPSProblem(Problem):
-    """The problem of searching in a graph from one node to another."""
-
     def __init__(self, initial, goal, graph):
         Problem.__init__(self, initial, goal)
         self.graph = graph
 
     def successor(self, A):
-        """Return a list of (action, result) pairs."""
         return [(B, B) for B in list(self.graph.get(A).keys())]
 
     def path_cost(self, cost_so_far, A, action, B):
-        return cost_so_far + (self.graph.get(A, B) or infinity)
+        # path_cost soportado en código base 
+        return cost_so_far + (self.graph.get(A, B) or float('inf'))
 
     def h(self, node):
-        """h function is straight-line distance from a node's state to goal."""
+        # Heurística distancia euclídea 
         locs = getattr(self.graph, 'locations', None)
         if locs:
             return int(distance(locs[node.state], locs[self.goal]))
         else:
-            return infinity
+            return float('inf')
